e65b978b770dc8202a4763784789c4092a21da81
William Griffin <williamgriffin989@gmail.com>
2015-02-18 17:25:45 -0500

    finished bid and art controllers

--- server/app/controllers/bids_controller.rb
+++ server/app/controllers/bids_controller.rb
@@ -1,2 +1,46 @@
 class BidsController < ApplicationController
+
+  def index
+    @bids = Bid.all
+    respond_to do |format|
+      format.json { json: @bids.to_json } 
+      format.html 
+    end
+  end
+
+  def create
+    @bid = Bid.create bid_params  
+    respond_to do |format|
+      format.json { json: @bid.to_json } 
+      format.html 
+    end
+  end
+
+  def update 
+    @bid = Bid.find params[:id]
+    @bid.update_attributes bid_params
+    respond_to do |format|
+      format.json { json: @bid.to_json } 
+      format.html 
+    end
+  end
+
+  def destroy
+    @bid = Bid.find params[:id]
+    @bid.delete
+    respond_to do |format|
+      format.json { render nothing: true } 
+      format.html 
+    end
+
+private 
+
+  def bid_params
+    params.require(:bid).permit(
+      :amount,
+      :bidder
+      )
+  end
 end
+
+

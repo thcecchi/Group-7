1bd2816e4ca175518e30cf8ed98ec7ab1ad39099
Tyler Cecchi <thcecchi@gmail.com>
2015-02-18 14:35:09 -0500

    added client folder

---
+++ client/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  var artCollection = new ArtCollection();
+
+  artCollection.fetch().then(function() {
+    var appView = new AppView({collection: artCollection})
+  });
+
+});
---
+++ client/models.js
@@ -0,0 +1,51 @@
+//Art model
+
+var ArtModel = Backbone.Model.extend({
+  urlRoot: '',
+  idAttribute: '_id',
+  defaults: function () {
+    return {
+      title: "generic title",
+      description: "generic description",
+      artist: "random artist",
+      image: "http://www.fillmurray.com/200/200",
+      dimensions: "generic dimensions"
+    };
+  },
+  initialize: function () {
+    console.log('ArtModel was created');
+  }
+})
+
+//Auction model
+
+var AuctionModel = Backbone.Model.extend({
+  urlRoot: '',
+  idAttribute: '_id',
+  defaults: function () {
+    return {
+      startx: "generic time",
+      endx: "generic time",
+      startingbid: "generic amount"
+    };
+  },
+  initialize: function () {
+    console.log('AuctionModel was created');
+  }
+})
+
+//Bid model
+
+var BidModel = Backbone.Model.extend({
+  urlRoot: '',
+  idAttribute: '_id',
+  defaults: function () {
+    return {
+      amount: "generic amount",
+      bidder: "generic name"
+    };
+  },
+  initialize: function () {
+    console.log('BidModel was created');
+  }
+})

154f80aa02bbeb9951d089c9cb7e434946faab0d
raveve <raven.askew@gmail.com>
2015-02-19 14:40:03 -0500

    Minor changes to template.

--- client/js/templates.js
+++ client/js/templates.js
@@ -1,22 +1,18 @@
 var template = {};
 
 template.homeItem = [
-  '<div class="col-md-3">',
+  '<article class="col-md-3">',
   '<div class="backimg" style="background-image: url("<%= image %>")">',
   '<h3><%= artist %></h3>',
   '<h3><%= title %></h3>',
   '<h3><%= bid %></h3>',
   '</div>',
-  '</div>',
-  '<div class="col-md-3 col-md-offset-3">',
-  '<h3><%= title %></h3>',
-  '<img src="<%= image %>">',
-  '</div>',
-  '<div class="col-md-3">',
+  '</article>',
+  '<article class="col-md-3">',
   '<h3><%= artist %></h3>',
   '<p><%= description %></p>',
   '<p><%= dimensions %></p>',
   '<h3><button>Bid</button></h3>',
   '<h4><button class="delete-item">Delete Item</button></h4>',
-  '</div>'
+  '</article>'
 ].join("");

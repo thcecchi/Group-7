8b22e115972afc102fa5b57d6715e05df88c4254
Ryan Mitchell <ryan@napaswdev.com>
2015-02-20 12:18:02 -0500

    Switch image upload to url string field.

--- server/app/models/art.rb
+++ server/app/models/art.rb
@@ -1,5 +1,3 @@
 class Art < ActiveRecord::Base
-  has_attached_file :image, :styles => { large: "1600x1600>", medium: "300x300>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :auctions, dependent: :destroy
 end
